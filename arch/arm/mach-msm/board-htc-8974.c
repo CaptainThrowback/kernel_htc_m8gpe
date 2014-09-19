50785576931f2aa0573d359cee02fc1aa59dbc22
Captain Throwback <captainthrowback@hotmail.com>
2014-09-19 12:36:19 -0400

    Fix UMS mounting in recovery

--- arch/arm/mach-msm/board-htc-8974.c
+++ arch/arm/mach-msm/board-htc-8974.c
@@ -370,9 +370,7 @@ static struct android_usb_platform_data android_usb_pdata = {
 	.usb_diag_interface = "diag",
 	.fserial_init_string = "smd:modem,tty,tty:autobot,tty:serial,tty:autobot,tty:acm",
 
-	.nluns = 1,
-	.cdrom_lun = 0x1,
-	.vzw_unmount_cdrom = 0,
+	.nluns = 2,
 };
 
 static struct platform_device android_usb_device = {
@@ -390,16 +388,12 @@ static void htc_8974_add_usb_devices(void)
 	if (board_mfg_mode() == 0) {
 #ifdef CONFIG_MACH_DUMMY
 		android_usb_pdata.nluns = 2;
-		android_usb_pdata.cdrom_lun = 0x2;
 #elif defined(CONFIG_MACH_DUMMY)
 		android_usb_pdata.nluns = 2;
-		android_usb_pdata.cdrom_lun = 0x2;
 #elif defined(CONFIG_MACH_MEC_WHL)
 		android_usb_pdata.nluns = 2;
-		android_usb_pdata.cdrom_lun = 0x2;
 #else
 		android_usb_pdata.nluns = 1;
-		android_usb_pdata.cdrom_lun = 0x1;
 #endif
 
 	}
@@ -407,9 +401,7 @@ static void htc_8974_add_usb_devices(void)
 	android_usb_pdata.product_id	= 0x061A;
 #elif defined(CONFIG_MACH_DUMMY)
 	android_usb_pdata.product_id	= 0x0616;
-	android_usb_pdata.vzw_unmount_cdrom = 1;
 	android_usb_pdata.nluns = 2;
-        android_usb_pdata.cdrom_lun = 0x3;
 #elif defined(CONFIG_MACH_DUMMY)
 	android_usb_pdata.product_id	= 0x0623;
 #elif defined(CONFIG_MACH_DUMMY)
