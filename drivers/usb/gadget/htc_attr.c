b2d56695c44dad4d71fd4f134a544c4174c8d020
Captain Throwback <captainthrowback@hotmail.com>
2014-09-25 12:55:52 -0400

    Random fixes and updates + cleanup

--- drivers/usb/gadget/htc_attr.c
+++ drivers/usb/gadget/htc_attr.c
@@ -410,11 +410,6 @@ int android_switch_function(unsigned func)
 		return 0;
 	}
 
-	if (board_mfg_mode() == 2) {		
-		printk("[USB] recovery mode now accepts MTP + UMS or ADB + MTP + UMS combination\n");		
-		func &= (1 << USB_FUNCTION_MTP) | (1 << USB_FUNCTION_UMS) | (1 << USB_FUNCTION_ADB);		
-	}
-
 	val = htc_usb_get_func_combine_value();
 
 	pr_info(" %u, before %u\n", func, val);
@@ -1184,14 +1179,9 @@ static void setup_vendor_info(struct android_dev *dev) {
 		ANDROID_USB_ENABLE_FUNC(dev, conf, "diag");
 		ANDROID_USB_ENABLE_FUNC(dev, conf, "modem");
 		ANDROID_USB_ENABLE_FUNC(dev, conf, "rmnet");
-	} else if (board_mfg_mode() == 2) {
+	} else {
 		ANDROID_USB_ENABLE_FUNC(dev, conf, "mtp");
 		ANDROID_USB_ENABLE_FUNC(dev, conf, "mass_storage");
-	} else {
-		if (!rom_stockui) {
-			ANDROID_USB_ENABLE_FUNC(dev, conf, "mtp");
-			ANDROID_USB_ENABLE_FUNC(dev, conf, "mass_storage");
-		}
 	}
 
 	product = get_product(dev, &conf->enabled_functions);
